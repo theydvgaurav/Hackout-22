b055c14fca428d41fbb76e1520aba202960b6fa8
sharma <anujjangra2002@gmail.com>
2022-10-14 23:47:28 +0530

    Add Folder Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { StoreProvider } from 'easy-peasy';
+import store from './Store/configureStore';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <StoreProvider store={store}>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+          {/* <Route path='/' element={<LandingPage />} exact />
+          <Route path='/login' element={<LoginScreen />} />
+          <Route path='/register' element={<SignupScreen />} />
+          <Route path='/notes' element={<MyNotes />} />
+          <Route path='/newNote' element={<NewNote />} />
+          <Route path='/editNote/:id' element={<EditNote />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </StoreProvider>
   );
 }
 
